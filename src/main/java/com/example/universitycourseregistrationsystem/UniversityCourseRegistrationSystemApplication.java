291b2b496c54de41094138f40c32752c69feec09
Abhijeet Deb Nath <deb.nath.3.1416@gmail.com>
2026-02-04 01:06:09 +0600

    beginning

---
+++ src/main/java/com/example/universitycourseregistrationsystem/UniversityCourseRegistrationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.universitycourseregistrationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityCourseRegistrationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversityCourseRegistrationSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/universitycourseregistrationsystem/UniversityCourseRegistrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.universitycourseregistrationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityCourseRegistrationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
