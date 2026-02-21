d2a5a049cd4bb114048d4f654f089639cc26a188
Abhijeet Deb Nath <deb.nath.3.1416@gmail.com>
2026-02-21 20:52:25 +0600

    Add ActiveProfiles test annotation for H2 database

--- src/test/java/com/example/universitycourseregistrationsystem/UniversityCourseRegistrationSystemApplicationTests.java
+++ src/test/java/com/example/universitycourseregistrationsystem/UniversityCourseRegistrationSystemApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.universitycourseregistrationsystem;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class UniversityCourseRegistrationSystemApplicationTests {
 
     @Test
